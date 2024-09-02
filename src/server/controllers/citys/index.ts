c89539b825f7b25441f7db63321a01a0f5abaa9e
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-09-02 19:33:31 -0400

    feat: :sparkles: start config jest test

--- src/server/controllers/citys/index.ts
+++ src/server/controllers/citys/index.ts
@@ -1,7 +1,13 @@
 import * as create from "./Create";
 import * as getAll from "./GetAll";
+import * as getById from "./GetById";
+import * as update from "./UpdateById";
+import * as deleteById from "./DeleteById";
 
 export const CitysController = {
   ...create,
   ...getAll,
+  ...getById,
+  ...update,
+  ...deleteById,
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 import { CitysController } from "../controllers";
 import { createValidation } from "../controllers/citys/Create";
 import { getAllValidation } from "../controllers/citys/GetAll";
+import { getByIdValidation } from "../controllers/citys/GetById";
+import { UpdateByIdValidation } from "../controllers/citys/UpdateById";
+import { deleteByIdValidation } from "../controllers/citys/DeleteById";
 
 const router = Router();
 
@@ -11,5 +14,8 @@ router.get("/", (req, res) => {
 
 router.get("/cidades", getAllValidation, CitysController.getAll);
 router.post("/cidades", createValidation, CitysController.create);
+router.get("/cidades/:id", getByIdValidation, CitysController.getById);
+router.put("/cidades/:id", UpdateByIdValidation, CitysController.UpdateById);
+router.delete("/cidades/:id", deleteByIdValidation, CitysController.deleteById);
 
 export { router };
